a8ff4dbf7ed1638fbb07800d620bb899cebcdf78
root <root@kali.kali>
2021-01-31 20:28:03 +0530

    Changes

---
+++ Client/build/precache-manifest.5050ac87d9d372a0a2a498cca0efe15a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88ad57057960ab42c583e8d1736d575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7281249cbe9f3cc8685",
+    "url": "/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "47b7749147fd4235f7e3",
+    "url": "/static/css/main.67f04ca0.chunk.css"
+  },
+  {
+    "revision": "c7281249cbe9f3cc8685",
+    "url": "/static/js/2.0c14840e.chunk.js"
+  },
+  {
+    "revision": "47b7749147fd4235f7e3",
+    "url": "/static/js/main.c05eec07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://PavanRangisetti:sk5UfBR5K8bgJgrC@chat-application.cceuv.mongodb.net/test?retryWrites=true&w=majority'
+}
